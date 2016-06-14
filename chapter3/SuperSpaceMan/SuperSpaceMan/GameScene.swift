01e0d28784467b9c38490347f2a187f51772f880
Wesley Matlock <wes@insoc.net>
2016-06-14 12:15:28 -0600

    Code changes to make the code Swifty.

--- chapter3/SuperSpaceMan/SuperSpaceMan/GameScene.swift
+++ chapter3/SuperSpaceMan/SuperSpaceMan/GameScene.swift
@@ -2,11 +2,11 @@ import SpriteKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    var backgroundNode : SKSpriteNode?
-    var playerNode : SKSpriteNode?
-    var orbNode : SKSpriteNode?
+    let backgroundNode = SKSpriteNode(imageNamed: "Background")
+    let playerNode = SKSpriteNode(imageNamed: "Player")
+    let orbNode = SKSpriteNode(imageNamed: "PowerUp")
     
-    let CollisionCategoryPlayer     : UInt32 = 0x1 << 1
+    let CollisionCategoryPlayer : UInt32 = 0x1 << 1
     let CollisionCategoryPowerUpOrbs : UInt32 = 0x1 << 2
 
     required init?(coder aDecoder: NSCoder) {
@@ -27,50 +27,47 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         isUserInteractionEnabled = true
         
         // adding the background
-        backgroundNode = SKSpriteNode(imageNamed: "Background")
-        backgroundNode!.size.width = self.frame.size.width
-        backgroundNode!.anchorPoint = CGPoint(x: 0.5, y: 0.0)
-        backgroundNode!.position = CGPoint(x: size.width / 2.0, y: 0.0)
-        addChild(backgroundNode!)
+        backgroundNode.size.width = frame.size.width
+        backgroundNode.anchorPoint = CGPoint(x: 0.5, y: 0.0)
+        backgroundNode.position = CGPoint(x: size.width / 2.0, y: 0.0)
+        addChild(backgroundNode)
         
         // add the player
-        playerNode = SKSpriteNode(imageNamed: "Player")
+        playerNode.position = CGPoint(x: size.width / 2.0, y: 80.0)
         
-        playerNode!.physicsBody =
-            SKPhysicsBody(circleOfRadius: playerNode!.size.width / 2)
-        playerNode!.physicsBody!.isDynamic = true
+        playerNode.physicsBody = SKPhysicsBody(circleOfRadius: playerNode.size.width / 2)
+        playerNode.physicsBody?.isDynamic = true
         
-        playerNode!.position = CGPoint(x: size.width / 2.0, y: 80.0)
-        playerNode!.physicsBody!.linearDamping = 1.0
-        playerNode!.physicsBody!.allowsRotation = false
-        playerNode!.physicsBody!.categoryBitMask = CollisionCategoryPlayer
-        playerNode!.physicsBody!.contactTestBitMask = CollisionCategoryPowerUpOrbs
-        playerNode!.physicsBody!.collisionBitMask = 0
-        addChild(playerNode!)
+        playerNode.position = CGPoint(x: size.width / 2.0, y: 80.0)
+        playerNode.physicsBody?.linearDamping = 1.0
+        playerNode.physicsBody?.allowsRotation = false
+        playerNode.physicsBody?.categoryBitMask = CollisionCategoryPlayer
+        playerNode.physicsBody?.contactTestBitMask = CollisionCategoryPowerUpOrbs
+        playerNode.physicsBody?.collisionBitMask = 0
+        addChild(playerNode)
         
-        orbNode = SKSpriteNode(imageNamed: "PowerUp")
-        orbNode!.position = CGPoint(x: 150.0, y: size.height - 25)
-        orbNode!.physicsBody =
-            SKPhysicsBody(circleOfRadius: orbNode!.size.width / 2)
-        orbNode!.physicsBody!.isDynamic = false
-        orbNode!.physicsBody!.categoryBitMask = CollisionCategoryPowerUpOrbs
-        orbNode!.physicsBody!.collisionBitMask = 0
-        orbNode!.name = "POWER_UP_ORB"
-        addChild(orbNode!)
+        orbNode.position = CGPoint(x: 150.0, y: size.height - 25)
+        orbNode.physicsBody = SKPhysicsBody(circleOfRadius: orbNode.size.width / 2)
+        orbNode.physicsBody?.isDynamic = false
+        orbNode.physicsBody?.categoryBitMask = CollisionCategoryPowerUpOrbs
+        orbNode.physicsBody?.collisionBitMask = 0
+        orbNode.name = "POWER_UP_ORB"
+        addChild(orbNode)
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        playerNode!.physicsBody!.applyImpulse(CGVector(dx: 0.0, dy: 40.0))
+        playerNode.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: 40.0))
     }
 
     func didBegin(_ contact: SKPhysicsContact) {
         
-        let nodeB = contact.bodyB.node!
+        let nodeB = contact.bodyB.node
         
-        if nodeB.name == "POWER_UP_ORB" {
+        if nodeB?.name == "POWER_UP_ORB" {
             
-            nodeB.removeFromParent()
+            nodeB?.removeFromParent()
         }
+        
     }
 }
