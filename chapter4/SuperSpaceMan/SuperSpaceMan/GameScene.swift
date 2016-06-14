958d51300237d1348fc2059e3adb90cca15b28c8
Wesley Matlock <wes@insoc.net>
2016-06-14 16:25:13 -0600

    Updated the codebase to be even more swifter.

--- chapter4/SuperSpaceMan/SuperSpaceMan/GameScene.swift
+++ chapter4/SuperSpaceMan/SuperSpaceMan/GameScene.swift
@@ -1,17 +1,17 @@
 import SpriteKit
 import CoreMotion
 
-class GameScene: SKScene, SKPhysicsContactDelegate {
+class GameScene: SKScene {
     
-    var backgroundNode : SKSpriteNode?
-    var foregroundNode  : SKSpriteNode?
-    var playerNode : SKSpriteNode?
+    let backgroundNode = SKSpriteNode(imageNamed: "Background")
+    let playerNode = SKSpriteNode(imageNamed: "Player")
+    let foregroundNode = SKSpriteNode()
     
     var impulseCount = 4
     let coreMotionManager = CMMotionManager()
     var xAxisAcceleration : CGFloat = 0.0
     
-    let CollisionCategoryPlayer     : UInt32 = 0x1 << 1
+    let CollisionCategoryPlayer      : UInt32 = 0x1 << 1
     let CollisionCategoryPowerUpOrbs : UInt32 = 0x1 << 2
 
     required init?(coder aDecoder: NSCoder) {
@@ -32,33 +32,29 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         isUserInteractionEnabled = true
         
         // adding the background
-        backgroundNode = SKSpriteNode(imageNamed: "Background")
-        backgroundNode!.size.width = self.frame.size.width
-        backgroundNode!.anchorPoint = CGPoint(x: 0.5, y: 0.0)
-        backgroundNode!.position = CGPoint(x: size.width / 2.0, y: 0.0)
-        addChild(backgroundNode!)
+        backgroundNode.size.width = frame.size.width
+        backgroundNode.anchorPoint = CGPoint(x: 0.5, y: 0.0)
+        backgroundNode.position = CGPoint(x: size.width / 2.0, y: 0.0)
+        addChild(backgroundNode)
         
-        foregroundNode = SKSpriteNode()
-        addChild(foregroundNode!)
+        addChild(foregroundNode)
         
         // add the player
-        playerNode = SKSpriteNode(imageNamed: "Player")
+        playerNode.position = CGPoint(x: size.width / 2.0, y: 80.0)
         
-        playerNode!.physicsBody =
-            SKPhysicsBody(circleOfRadius: playerNode!.size.width / 2)
-        playerNode!.physicsBody!.isDynamic = false
+        playerNode.physicsBody = SKPhysicsBody(circleOfRadius: playerNode.size.width / 2)
+        playerNode.physicsBody?.isDynamic = true
         
-        playerNode!.position = CGPoint(x: size.width / 2.0, y: 80.0)
-        playerNode!.physicsBody!.linearDamping = 1.0
-        playerNode!.physicsBody!.allowsRotation = false
-        playerNode!.physicsBody!.categoryBitMask = CollisionCategoryPlayer
-        playerNode!.physicsBody!.contactTestBitMask = CollisionCategoryPowerUpOrbs
-        playerNode!.physicsBody!.collisionBitMask = 0
+        playerNode.position = CGPoint(x: size.width / 2.0, y: 80.0)
+        playerNode.physicsBody?.linearDamping = 1.0
+        playerNode.physicsBody?.allowsRotation = false
+        playerNode.physicsBody?.categoryBitMask = CollisionCategoryPlayer
+        playerNode.physicsBody?.contactTestBitMask = CollisionCategoryPowerUpOrbs
+        playerNode.physicsBody?.collisionBitMask = 0
         
-        foregroundNode!.addChild(playerNode!)
+        foregroundNode.addChild(playerNode)
         
-        var orbNodePosition = CGPoint(x: playerNode!.position.x,
-            y: playerNode!.position.y + 100)
+        var orbNodePosition = CGPoint(x: playerNode.position.x, y: playerNode.position.y + 100)
         
         for _ in 0...19 {
             
@@ -67,16 +63,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             orbNodePosition.y += 140
             orbNode.position = orbNodePosition
             orbNode.physicsBody = SKPhysicsBody(circleOfRadius: orbNode.size.width / 2)
-            orbNode.physicsBody!.isDynamic = false
+            orbNode.physicsBody?.isDynamic = false
                 
-            orbNode.physicsBody!.categoryBitMask = CollisionCategoryPowerUpOrbs
-            orbNode.physicsBody!.collisionBitMask = 0
+            orbNode.physicsBody?.categoryBitMask = CollisionCategoryPowerUpOrbs
+            orbNode.physicsBody?.collisionBitMask = 0
             orbNode.name = "POWER_UP_ORB"
                 
-            foregroundNode!.addChild(orbNode)
+            foregroundNode.addChild(orbNode)
         }
         
-        orbNodePosition = CGPoint(x: playerNode!.position.x + 50, y: orbNodePosition.y)
+        orbNodePosition = CGPoint(x: playerNode.position.x + 50, y: orbNodePosition.y)
         
         for _ in 0...19 {
             
@@ -85,21 +81,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             orbNodePosition.y += 140
             orbNode.position = orbNodePosition
             orbNode.physicsBody = SKPhysicsBody(circleOfRadius: orbNode.size.width / 2)
-            orbNode.physicsBody!.isDynamic = false
-            
-            orbNode.physicsBody!.categoryBitMask = CollisionCategoryPowerUpOrbs
-            orbNode.physicsBody!.collisionBitMask = 0
+            orbNode.physicsBody?.isDynamic = false
+    
+            orbNode.physicsBody?.categoryBitMask = CollisionCategoryPowerUpOrbs
+            orbNode.physicsBody?.collisionBitMask = 0
             orbNode.name = "POWER_UP_ORB"
             
-            foregroundNode!.addChild(orbNode)
+            foregroundNode.addChild(orbNode)
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 
-        if !playerNode!.physicsBody!.isDynamic {
+        if !playerNode.physicsBody!.isDynamic {
             
-            playerNode!.physicsBody!.isDynamic = true
+            playerNode.physicsBody?.isDynamic = true
             
             coreMotionManager.accelerometerUpdateInterval = 0.3
             coreMotionManager.startAccelerometerUpdates(to: OperationQueue(), withHandler: {
@@ -118,11 +114,42 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         if impulseCount > 0 {
             
-            playerNode!.physicsBody!.applyImpulse(CGVector(dx: 0.0, dy: 40.0))
+            playerNode.physicsBody!.applyImpulse(CGVector(dx: 0.0, dy: 40.0))
             impulseCount -= 1
         }
     }
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+        if playerNode.position.y >= 180.0 {
+            
+            backgroundNode.position = CGPoint(x: backgroundNode.position.x, y: -((playerNode.position.y - 180.0)/8))
+            foregroundNode.position = CGPoint(x: foregroundNode.position.x, y: -(playerNode.position.y - 180.0))
+        }
+    }
+    
+    override func didSimulatePhysics() {
+        
+        playerNode.physicsBody!.velocity = CGVector(dx: self.xAxisAcceleration * 380.0,dy: playerNode.physicsBody!.velocity.dy)
+        
+        if playerNode.position.x < -(playerNode.size.width / 2) {
+            
+            playerNode.position = CGPoint(x: size.width - playerNode.size.width / 2, y: playerNode.position.y);
+        }
+        else if playerNode.position.x > self.size.width {
+            
+            playerNode.position = CGPoint(x: playerNode.size.width / 2, y: playerNode.position.y);
+        }
+    }
+    
+    deinit {
+        
+        self.coreMotionManager.stopAccelerometerUpdates()
+    }
+}
 
+extension GameScene: SKPhysicsContactDelegate {
+    
     func didBegin(_ contact: SKPhysicsContact) {
         
         let nodeB = contact.bodyB.node!
@@ -132,42 +159,5 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             impulseCount += 1
             nodeB.removeFromParent()
         }
-    }
-    
-    override func update(_ currentTime: TimeInterval) {
-        
-        if playerNode!.position.y >= 180.0 {
-            
-            backgroundNode!.position =
-                CGPoint(x: backgroundNode!.position.x,
-                y: -((playerNode!.position.y - 180.0)/8))
-            foregroundNode!.position =
-                CGPoint(x: foregroundNode!.position.x,
-                y: -(playerNode!.position.y - 180.0))
-        }
-    }
-    
-    override func didSimulatePhysics() {
-        
-        self.playerNode!.physicsBody!.velocity =
-            CGVector(dx: self.xAxisAcceleration * 380.0,
-                dy: self.playerNode!.physicsBody!.velocity.dy)
-        
-        if playerNode!.position.x < -(playerNode!.size.width / 2) {
-            
-            playerNode!.position =
-                CGPoint(x: size.width - playerNode!.size.width / 2,
-                    y: playerNode!.position.y);
-        }
-        else if self.playerNode!.position.x > self.size.width {
-            
-            playerNode!.position = CGPoint(x: playerNode!.size.width / 2,
-                y: playerNode!.position.y);
-        }
-    }
-    
-    deinit {
-        
-        self.coreMotionManager.stopAccelerometerUpdates()
     }
 }
